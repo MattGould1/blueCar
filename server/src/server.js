4f6e39a4aed520d3bfec8b89490fcc8ccdc34c8e
matt <matt@saowapan.com>
2017-10-01 09:14:40 +0100

    noble

--- server/src/server.js
+++ server/src/server.js
@@ -5,17 +5,36 @@ import _ from 'underscore'
 
 import noble from 'noble'
 
+import BTSP from 'bluetooth-serial-port'
 
-noble.state = 'poweredOn'
-var serviceUUIDs = []
-noble.startScanning(serviceUUIDs, false, (err) => {
-    console.log(serviceUUIDs);
+var serial = new BTSP.BluetoothSerialPort();
+ 
+serial.on('found', function(address, name) {
+ 
+    // you might want to check the found address with the address of your
+    // bluetooth enabled Arduino device here.
+ 
+    serial.findSerialPortChannel(address, function(channel) {
+        serial.connect(bluetoothAddress, channel, function() {
+            console.log('connected');
+            process.stdin.resume();
+            process.stdin.setEncoding('utf8');
+            console.log('Press "1" or "0" and "ENTER" to turn on or off the light.')
+ 
+            process.stdin.on('data', function (data) {
+                serial.write(data);
+            });
+ 
+            serial.on('data', function(data) {
+                console.log('Received: ' + data);
+            });
+        }, function () {
+            console.log('cannot connect');
+        });
+    });
 });
-
-noble.on('discover', (peripheral) => {
-    console.log(peripheral)
-});
-
+ 
+serial.inquire();
 /*
     Our restful routes will be housed here
 */
